e9aaf99e08330eee9d30380d7b7a6efd3a1807c7
saabirah <saabirah1993@gmail.com>
2021-05-20 11:33:36 +0100

    angular routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PasswordComponent } from './components/password/password.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+{
+  path:'login',
+  component: LoginComponent
+},
+
+{
+  path:'signup',
+  component: SignupComponent
+},
+
+{
+  path:'profile',
+  component: ProfileComponent
+},
+
+{
+  path:'request-password-reset ',
+  component: RequestResetComponent
+},
+
+{
+  path:'response-password-reset',
+  component: ResponseResetComponent
+},
+]
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations:[],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
+// a supprimer
+/*const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { }*/
